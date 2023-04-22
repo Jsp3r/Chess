e089fae030c2a35138253d2f848399f0332661f4
UBSITAsminJasperAPPDEV1-IDB1_222308 <20215663@s.ubaguio.edu>
2023-04-22 13:14:52 +0800

    nav and components

--- LearnChess/src/app/app-routing.module.ts
+++ LearnChess/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { OpeningComponent } from './Components/opening/opening.component';
+import { StrategyComponent } from './Components/strategy/strategy.component';
+import { AboutComponent } from './Components/about/about.component';
+import { LoginComponent } from './Components/login/login.component';
+import { SignupComponent } from './Components/signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'opening', component: OpeningComponent},
+  {path: 'strategy', component: StrategyComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
